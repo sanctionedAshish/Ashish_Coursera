074847b2f98a33eea0188791783000b7e1964268
sanctionedAshish <squadashish138@gmail.com>
2020-08-22 12:20:17 +0530

    Added codes to analyze statistics

--- stats.c
+++ stats.c
@@ -35,11 +35,25 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+	int median, mean, maximum, minimum;                                
 
+	printf("<<< Intro Embedded Systems >>>\n");
+	printf("<< Week 1 - C1M1 >>\n\n");
+	printf("<Data Array>\n");
+	print_array(test, SIZE);
+
+	printf("\n> Ordering array...\n");
+	sort_array(test, SIZE);
+	printf("<Ordered Data Array>\n");
+	print_array(test, SIZE);
+
+	printf("\n> Calculate and show statistics...\n");
+	median = find_median(test, SIZE);
+	mean = find_mean(test, SIZE);
+	maximum = find_maximum(test, SIZE);
+	minimum = find_minimum(test, SIZE);
+  print_statistics(median, mean, maximum, minimum);
 }
-
 
 /**
  * @brief Print data array statistics.
@@ -54,10 +68,13 @@ void main() {
  *
  * @return void
  */
-
 void print_statistics(int median, int mean, int maximum, int minimum) {
+	 printf("<Statistics>\n");
+	 printf("> Median: %5d\n", median);
+	 printf("> Mean: %7d\n", mean);
+	 printf("> Maximum: %4d\n", maximum);
+	 printf("> Minimum: %4d\n", minimum);
 }
-
 
 /**
  * @brief Prints array to screen.
@@ -70,6 +87,16 @@ void print_statistics(int median, int mean, int maximum, int minimum) {
  * @return void
  */
 void print_array(unsigned char *data, int len) {
+	printf("[ ");
+	for (int i = 0; i < len; ++i)
+	{
+		if (i < len-1) {
+			printf("%d, ", data[i]);
+		} else {
+			printf("%d", data[i]);
+		}
+	}
+	printf(" ]\n");
 }
 
 /**
@@ -83,10 +110,14 @@ void print_array(unsigned char *data, int len) {
  *
  * @return The data median value
  */
-
-
 int find_median(unsigned char *data, int len) {
+	if (len % 2 == 0) {
+		return ( data[(len/2)] + data[(len/2)+1] ) / 2;
+	} else {
+		return data[(len/2)+1];
+	}
 }
+
 /**
  * @brief Calculate array data mean value.
  *
@@ -99,7 +130,14 @@ int find_median(unsigned char *data, int len) {
  * @return The data mean value
  */
 int find_mean(unsigned char *data, int len) {
+	int sum = 0;
+	for (int i = 0; i < len; ++i)
+	{
+		sum += data[i];
+	}
+	return sum / len;
 }
+
 /**
  * @brief Calculate array data maximum value.
  *
@@ -112,6 +150,7 @@ int find_mean(unsigned char *data, int len) {
  * @return The data maximum value
  */
 int find_maximum(unsigned char *data, int len) {
+	return data[0];
 }
 
 /**
@@ -125,8 +164,8 @@ int find_maximum(unsigned char *data, int len) {
  *
  * @return The data minimum value
  */
-
 int find_minimum(unsigned char *data, int len) {
+	return data[len-1];
 }
 
 /**
@@ -141,9 +180,18 @@ int find_minimum(unsigned char *data, int len) {
  * @param len 	Array length	
  *
  * @return void
+sorted in decreasing order
  */
-
 void sort_array(unsigned char *data, int len) {
+	int aux;
+	for (int i = 0; i < len-1; i++) {
+		for (int j = i+1; j < len; ++j)
+		{
+			if (data[i] < data[j]) {
+				aux = data[i];
+				data[i] = data[j];
+				data[j] = aux; 
+			}
+		}
+	}
 }
-
-/* Add other Implementation File Code Here */
